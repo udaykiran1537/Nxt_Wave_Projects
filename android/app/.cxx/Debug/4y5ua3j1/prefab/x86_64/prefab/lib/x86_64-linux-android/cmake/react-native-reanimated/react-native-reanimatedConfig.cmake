c7a5eba84d70bea04baf4f2d209c73cece22d12f
uday <vappangiuday@gmail.com>
2025-08-20 09:57:07 +0530

    initial commit changes

--- android/app/.cxx/Debug/4y5ua3j1/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/Users/banalapradeep/Desktop/NxtWave_Training/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/n4k6ty2i/obj/x86_64/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/banalapradeep/Desktop/NxtWave_Training/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
